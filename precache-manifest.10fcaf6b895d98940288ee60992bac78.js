34fd2057be9d362a59e8d8fa226567ad8145916e
Katorin <ducmanh.tran2904@gmail.com>
2020-08-15 14:00:05 +0700

    upload

---
+++ precache-manifest.10fcaf6b895d98940288ee60992bac78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae655a49357ca831991d502af87dd75",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0bcabc260be9122d557f",
+    "url": "./static/css/main.92255b88.chunk.css"
+  },
+  {
+    "revision": "27c8e20fed41358bba95",
+    "url": "./static/js/2.4613981b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.4613981b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bcabc260be9122d557f",
+    "url": "./static/js/main.c1705799.chunk.js"
+  },
+  {
+    "revision": "985fbd84deadeb4d38cc",
+    "url": "./static/js/runtime-main.6f8f0865.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
